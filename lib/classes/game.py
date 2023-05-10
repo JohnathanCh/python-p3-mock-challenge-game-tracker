ac0280a527792f05379773422d3d15a11462c989
JohnathanCh <johnchriest@gmail.com>
2023-05-10 16:47:50 -0700

    done2

--- lib/classes/game.py
+++ lib/classes/game.py
@@ -2,31 +2,38 @@ import pdb
 
 class Game:
     def __init__(self, title):
-        if len(title) == 0:
-            raise Exception("Title must be longer than 0")
-        else:
-            self._title = title
-            self._results = []
-            self._players = []
+        # if len(title) == 0:
+        #     raise Exception("Title must be longer than 0")
+        # else:
+        self.title = title
+        self._results = []
+        self._players = []
             
     @property
     def title(self):
         return self._title
     
+    @title.setter
+    def title(self, title):
+        if title and isinstance(title, str) and not hasattr(self, 'title'):
+            self._title = title
+        else:
+            raise Exception("Title must be longer than 0")
+    
     def results(self, new_result=None):
         from classes.result import Result
-        if new_result:
+        if new_result and isinstance(new_result, Result):
             self._results.append(new_result)
         return self._results
     
     def players(self, new_player=None):
         from classes.player import Player
-        if new_player:
+        if new_player and isinstance(new_player, Player):
             self._players.append(new_player)
         return self._players
     
     def average_score(self, player):
         # pdb.set_trace()
-        scores = [result.score for result in player.results()]
+        scores = [result.score for result in player.results() if result.player is player]
         avg = sum(scores)/len(scores)
         return avg
--- lib/classes/player.py
+++ lib/classes/player.py
@@ -5,13 +5,10 @@ class Player:
     all = []
 
     def __init__(self, username):
-        if 2 <= len(username) <= 16:
-            self.username = username
-            self._results = []
-            self._games_played = []
-            Player.all.append(self)
-        else:
-            raise Exception("INVALID USERNAME: Username must be between 2 and 16 characters")
+        self.username = username
+        self._results = []
+        self._games_played = []
+        Player.all.append(self)
         
     @property
     def username(self):
@@ -26,23 +23,21 @@ class Player:
         
     def results(self, new_result=None):
         from classes.result import Result
-        if new_result is not None:
+        if new_result and isinstance(new_result, Result):
             self._results.append(new_result)
         return self._results
         
     
     def games_played(self, new_game=None):
         from classes.game import Game
-        if new_game:
+        if new_game and isinstance(new_game, Game):
             self._games_played.append(new_game)
         return self._games_played
     
     def played_game(self, game):
-        # ipdb.set_trace()
         return game in self._games_played
     
     def num_times_played(self, game):
-        # ipdb.set_trace()
         return game.players().count(self)
     
     @classmethod
